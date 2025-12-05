02009de1fb5979e2f7f480c4b286132dc39854ba
Fahad Kabir <kabirkaife@gmail.com>
2025-12-05 21:32:58 +0000

    logo is gone

--- src/components/app-logo.tsx
+++ src/components/app-logo.tsx
@@ -2,7 +2,7 @@ import { Home } from 'lucide-react';
 
 export default function AppLogo() {
   return (
-    <div className="flex items-center gap-2 text-primary">
+    <div className="flex items-center gap-2 text-inherit">
       <Home className="h-6 w-6" />
       <span className="text-xl font-semibold font-headline">Neighbor Nexus</span>
     </div>
